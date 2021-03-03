e286d2ba70c846bbaa59c740991cacd6889e6bb0
Ubuntu <ubuntu@ip-172-31-43-108.us-east-2.compute.internal>
2021-03-03 04:02:46 +0000

    Outline

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Time::DATE_FORMATS[:datetime_jp] = '%Y年 %m月%d日'
+Date::DATE_FORMATS[:default] = '%Y年 %m月%d日'
---
+++ db/migrate/20210302073917_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :start_date
+      t.string :end_date
+      t.string :all_day
+
+      t.timestamps
+    end
+  end
+end
